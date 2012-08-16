b9a392c60a2d01eb743a1f0baf55b74dc6af32cf
Mitsutoshi Aoe <maoe@foldr.in>
2012-08-16 09:46:24 +0900

    Add test cases for withPrevE and differentE
    NOTE: Current implementation of differentE is broken.

--- tests/Test/Event.hs
+++ tests/Test/Event.hs
@@ -46,3 +46,22 @@ case_splitOnE = do
         ev2 <- eventFromList [[], [()], [], [], [()]]
         eventToSignal <$> splitOnE ev2 ev1
     result @?= [[], [[1,2,3,4]], [], [], [[5,6,7,8]]]
+
+case_withPrevE :: Assertion
+case_withPrevE = do
+    result <- networkToList 5 $ do
+        ev <- eventFromList [[1::Int], [2,3,4], [], [5,6], [7,8]]
+        eventToSignal <$> withPrevE 0 ev
+    result @?= [ [(1,0)]
+               , [(2,1),(3,2),(4,3)]
+               , []
+               , [(5,4),(6,5)]
+               , [(7,6),(8,7)]
+               ]
+
+case_differentE :: Assertion
+case_differentE = do
+    result <- networkToList 6 $ do
+        ev <- eventFromList [[1 :: Int], [1, 1, 2], [], [3, 3], [3], [3, 4]]
+        eventToSignal <$> differentE ev
+    result @?= [[1], [2], [], [3], [], [4]]
