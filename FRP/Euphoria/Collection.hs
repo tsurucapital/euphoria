e11cf813c2c564684424d0d6f1e393fd9385e94c
Alex Sayers <alex.sayers@gmail.com>
2015-08-28 11:06:53 +0900

    Make API backward-compatible with 0.6.0.1

--- FRP/Euphoria/Collection.hs
+++ FRP/Euphoria/Collection.hs
@@ -1,8 +1,35 @@
 
 -- | A re-export of FRP.Euphoria.Collection.Enum for API compatability.
+-- `Collection.Enum.mapToCollection` is renamed to `enummapToCollection`,
+-- and `Collection.Hashable.mapToCollection` is exported as
+-- `hashmapToCollection`.
 module FRP.Euphoria.Collection
     ( module FRP.Euphoria.Collection.Enum
+    , enummapToCollection
+    , hashmapToCollection
     ) where
 
-import FRP.Euphoria.Collection.Enum
+import Data.EnumMap.Lazy (EnumMap)
+import Data.HashMap.Strict (HashMap)
+import Data.Hashable (Hashable)
+
+import FRP.Euphoria.Collection.Enum hiding (mapToCollection)
+
+import qualified FRP.Euphoria.Collection.Enum     as Enum
+import qualified FRP.Euphoria.Collection.Hashable as Hashable
+
+import FRP.Euphoria.Event
+
+enummapToCollection
+    :: (Eq k, Eq a, Enum k, MonadSignalGen m)
+    => Discrete (EnumMap k a)
+    -> m (Collection k (Discrete a))
+enummapToCollection = Enum.mapToCollection
+
+hashmapToCollection
+    :: (Eq k, Eq a, Hashable k, MonadSignalGen m)
+    => Discrete (HashMap k a)
+    -> m (Collection k (Discrete a))
+hashmapToCollection = Hashable.mapToCollection
+
 
